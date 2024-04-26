f3ce33f1851787212a42517fed8ee1852dad2c66
Louie Chen <louieforcode@gmail.com>
2024-04-26 19:09:31 +0800

    feat: strapi add cuctom comment api

--- packages/strapi/src/api/comment/controllers/comment.ts
+++ packages/strapi/src/api/comment/controllers/comment.ts
@@ -4,4 +4,11 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::comment.comment');
+export default factories.createCoreController('api::comment.comment', ({ strapi }) => ({
+    async create(ctx) {
+      const { user } = ctx.state;
+      const { id } = user;
+      ctx.request.body.data.user = id;
+      return super.create(ctx);
+    },
+  }));
