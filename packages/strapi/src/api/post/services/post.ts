b3623d9561c181b7da70bed23c33b49d1502eaf2
Louie Chen <louieforcode@gmail.com>
2024-04-26 19:09:46 +0800

    feat: strapi add cuctom post api

--- packages/strapi/src/api/post/services/post.ts
+++ packages/strapi/src/api/post/services/post.ts
@@ -4,4 +4,34 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::post.post');
+export default factories.createCoreService('api::post.post', {
+    async likeOrFavor(ctx, type) {
+      const { id } = ctx.params;
+      const { user } = ctx.state;
+      const { id: userId } = user;
+  
+      const relation = type === "like" ? "liked_by" : "favored_by";
+  
+      const post = await strapi.entityService.findOne("api::post.post", id, {
+        populate: [relation],
+      });
+  
+      const relations = post[relation];
+  
+      const index = relations.findIndex((r) => r.id === userId);
+      let already = false;
+      if (index !== -1) {
+        relations.splice(index, 1);
+      } else {
+        already = true;
+        relations.push(userId);
+      }
+  
+      await strapi.entityService.update("api::post.post", id, {
+        data: { [relation]: relations },
+      });
+      ctx.body = {
+        data: already,
+      };
+    },
+  });
