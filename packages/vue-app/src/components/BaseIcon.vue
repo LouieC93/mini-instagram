8be656d938c2f3a6bd195171d30e23aecc4d3ef9
Louie Chen <louieforcode@gmail.com>
2024-04-29 19:04:22 +0800

    feat: add base icon component

---
+++ packages/vue-app/src/components/BaseIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>icon-{{ iconId }}</p>
+  <svg>
+    <use :href="`${sprite}#icon-${iconId}`"></use>
+  </svg>
+</template>
+<script setup lang="ts">
+import sprite from '../assets/icons/sprite.svg'
+
+withDefaults(
+  defineProps<{
+    iconId: string
+  }>(),
+  {
+    iconId: ''
+  }
+)
+</script>
