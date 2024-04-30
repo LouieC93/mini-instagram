850949f5ed9cf65bfca87314ed41e797fd9959d4
Louie Chen <louieforcode@gmail.com>
2024-04-30 17:57:09 +0800

    feat: add all routes

--- packages/vue-app/src/router/index.ts
+++ packages/vue-app/src/router/index.ts
@@ -1,8 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '@/views/HomePage.vue'
+import SearchPage from '@/views/SearchPage.vue'
+import ProfilePage from '@/views/ProfilePage.vue'
+import ProfileEditPage from '@/views/ProfileEditPage.vue'
+import LoginPage from '@/views/LoginPage.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: SearchPage
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfilePage
+  },
+  {
+    path: '/profile/edit',
+    name: 'profileEdit',
+    component: ProfileEditPage
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginPage
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: {
+      name: 'home'
+    }
+  }
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes
 })
 
 export default router
