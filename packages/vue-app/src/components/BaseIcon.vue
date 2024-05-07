6a81447b2aa3c05764bedda5ceccd3172aaaf251
Louie Chen <louieforcode@gmail.com>
2024-05-07 17:56:24 +0800

    feat: adjust component baseIcon

--- packages/vue-app/src/components/BaseIcon.vue
+++ packages/vue-app/src/components/BaseIcon.vue
@@ -4,14 +4,25 @@
   </svg>
 </template>
 <script setup lang="ts">
+import { computed } from 'vue'
 import sprite from '../assets/icons/sprite.svg'
 
-withDefaults(
+const props = withDefaults(
   defineProps<{
     iconId: string
+    size?: number
   }>(),
   {
-    iconId: ''
+    iconId: '',
+    size: 36
   }
 )
+const iconSize = computed(() => String(props.size) + 'px')
 </script>
+
+<style lang="scss">
+svg {
+  width: v-bind(iconSize);
+  height: v-bind(iconSize)
+}
+</style>
