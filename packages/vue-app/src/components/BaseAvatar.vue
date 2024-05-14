1d9119752536c4316c2c93128e6fe3e97794a5d5
Louie Chen <louieforcode@gmail.com>
2024-05-14 19:17:33 +0800

    feat: add avatar component

---
+++ packages/vue-app/src/components/BaseAvatar.vue
@@ -0,0 +1,28 @@
+<template>
+  <img :src="src" alt="avatar" />
+</template>
+<script setup lang="ts">
+import { computed } from 'vue'
+import defaultAvatar from '../assets/avatarDefault.svg'
+
+const props = withDefaults(
+  defineProps<{
+    src?: string
+    size?: number
+  }>(),
+  {
+    src: defaultAvatar,
+    size: 40
+  }
+)
+const avatarSize = computed(() => String(props.size) + 'px')
+</script>
+<style lang="scss" scoped>
+img {
+  width: v-bind(avatarSize);
+  height: v-bind(avatarSize);
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: center;
+}
+</style>
