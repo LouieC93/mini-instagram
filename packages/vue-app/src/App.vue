ba508655f40e7780ecba1b29ffbca9cfb8cdbfde
Louie Chen <louieforcode@gmail.com>
2024-05-27 16:32:39 +0800

    feat: add component LoginPage

--- packages/vue-app/src/App.vue
+++ packages/vue-app/src/App.vue
@@ -1,16 +1,22 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-import NavBar from './components/NavBar.vue'
-</script>
-
 <template>
-  <div id="content">
+  <div id="content" v-if="!isLoginPage">
     <header id="header">
       <NavBar />
     </header>
     <RouterView />
   </div>
+  <div id="content" class="full-page" v-else>
+    <RouterView />
+  </div>
 </template>
+<script lang="ts" setup>
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue';
+const route = useRoute()
+const isLoginPage = computed(() => route.name === 'login')
+</script>
 
 <style lang="scss" scoped>
 #content {
@@ -19,6 +25,9 @@ import NavBar from './components/NavBar.vue'
   grid-template-rows: auto 1fr;
   #header {
     box-shadow: 0 4px 12px #eeee;
+  }
+  &.full-page {
+    grid-template-rows: 1fr
   }
 }
 </style>
--- packages/vue-app/src/views/LoginPage.vue
+++ packages/vue-app/src/views/LoginPage.vue
@@ -1,3 +1,115 @@
 <template>
-    <main></main>
+  <main>
+    <img src="../assets/cover.jpeg" alt="cover" class="cover" />
+    <form class="form-container">
+      <div class="title">
+        <img src="../assets/logo.png" alt="" />
+        <h1>Mini Instagram</h1>
+      </div>
+      <input type="email" placeholder="Email" />
+      <input type="text" placeholder="Username" v-if="isNewUser" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Submit</button>
+      <p v-if="isNewUser">
+        Have an account?
+        <router-link :to="{}" @click="isNewUser = !isNewUser">Login</router-link>
+      </p>
+      <p v-else>
+        Don't have an account?
+        <router-link :to="{}" @click="isNewUser = !isNewUser">Sign up</router-link>
+      </p>
+      <div class="policy-container">
+        <input type="checkbox" id="policy" />
+        <label for="policy">
+          By signing up, you agree to our Terms , Privacy Policy and Cookies Policy
+        </label>
+      </div>
+    </form>
+  </main>
 </template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const isNewUser = ref(true)
+</script>
+<style lang="scss" scoped>
+main {
+  height: 100%;
+  min-height: 900px;
+  background: #f8f9fb;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: max(36px, 6dvw);
+  align-items: center;
+  > * {
+    height: 70%;
+    min-width: 450px;
+  }
+  .cover {
+    grid-column: 2 / 4;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .form-container {
+    grid-column: 4 / 6;
+    background: #fff;
+    padding: 60px 36px;
+    box-shadow: 0 4px 12px #eeee;
+    display: grid;
+    grid-auto-rows: auto;
+    border-radius: 28px;
+    align-content: center;
+    justify-items: center;
+    > input {
+      width: 100%;
+      margin-bottom: 36px;
+      background: #eee;
+      border-radius: 6px;
+      height: 48px;
+    }
+    > button {
+      width: 100%;
+      margin-bottom: 24px;
+      background: $blue;
+      font-size: 18px;
+      color: #fff;
+      border-radius: 12px;
+      padding: 12px 36px;
+    }
+    > p {
+      display: grid;
+      grid-template-columns: repeat(2, auto);
+      grid-gap: 6px;
+      margin-bottom: 24px;
+      > a {
+        color: $blue;
+      }
+    }
+    .title {
+      width: 100%;
+      padding-right: 2px;
+      margin-bottom: 48px;
+      display: grid;
+      grid-template-columns: repeat(2, auto);
+      justify-content: center;
+      align-items: center;
+      grid-gap: 18px;
+      img {
+        width: 48px;
+      }
+    }
+    .policy-container {
+      display: grid;
+      grid-template-columns: auto auto;
+      grid-gap: 12px;
+      align-items: center;
+      input {
+        width: 16px;
+        height: 16px;
+        border-radius: 6px;
+      }
+    }
+  }
+}
+</style>
